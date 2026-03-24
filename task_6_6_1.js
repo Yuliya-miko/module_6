381f1d4b79a2d5d1ae3d5f35506f35b334761796
Yuliya-miko <216386154+Yuliya-miko@users.noreply.github.com>
2026-03-24 21:48:05 +0100

    Добавлено задание 6.6.1

---
+++ task_6_6_1.js
@@ -0,0 +1,11 @@
+let stringPolidrom = "Ровод";
+let stringPolidromLowerCase = stringPolidrom.toLowerCase();
+let stringPolidromReverse = stringPolidromLowerCase
+  .split("")
+  .reverse()
+  .join("");
+if (stringPolidromLowerCase === stringPolidromReverse) {
+  console.log("Строка является полиндромом");
+} else {
+  console.log("Строка не является полиндромом");
+}
